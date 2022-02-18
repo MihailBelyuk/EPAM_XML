2a6e0278b91b690db6799c8358267e9ca29411a6
MihailBelyuk <Tamerlan123Sadist123>
2022-02-18 10:01:25 +0300

    Initial commit

---
+++ src/main/java/com/belyuk/second_project/builder/MedicineDomBuilder.java
@@ -0,0 +1,134 @@
+package com.belyuk.second_project.builder;
+
+
+import com.belyuk.second_project.entity.impl.AntibioticsImpl;
+import com.belyuk.second_project.entity.Medicine;
+import com.belyuk.second_project.entity.impl.VitaminsImpl;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class MedicineDomBuilder {
+
+  private Set<Medicine> medicineSet;
+  private DocumentBuilder documentBuilder;
+  private static final String NAME = "name";
+  private static final String MANUFACTURER = "manufacturer";
+  private static final String DOSAGE_FORM = "dosage_form";
+  private static final String AMOUNT = "amount";
+  private static final String ID = "id";
+  private static final String DOSAGE = "dosage";
+  private static final String PRICE = "price";
+  private static final String ISSUE_DATE = "issue_date";
+  private static final String EXPIRY_DATE = "expiry_date";
+  private static final String SOLUBILITY = "solubility";
+  private static final String SERIAL = "serial";
+  private static final String DATE_PATTERN = "dd.MM.yyyy";
+  private static final String VITAMINS = "vitamins";
+  private static final String ANTIBIOTICS = "antibiotics";
+
+  public MedicineDomBuilder() {
+    medicineSet = new HashSet<Medicine>();
+    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+    try {
+      documentBuilder = documentBuilderFactory.newDocumentBuilder();
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Set<Medicine> getMedicineSet() {
+    return medicineSet;
+  }
+
+  public void buildSetMedicine(String filename, String elementName) {
+    Document document;
+    try {
+      document = documentBuilder.parse(filename);
+      Element root = document.getDocumentElement();
+      NodeList medicineList = root.getElementsByTagName(elementName);
+      for (int i = 0; i < medicineList.getLength(); i++) {
+        Element medicineElement = (Element) medicineList.item(i);
+        if (medicineElement.getTagName().equals(VITAMINS)) {
+          Medicine medicine = buildVitamins(medicineElement);
+          medicineSet.add(medicine);
+        }
+        if (medicineElement.getTagName().equals(ANTIBIOTICS)) {
+          Medicine medicine = buildAntibiotics(medicineElement);
+          medicineSet.add(medicine);
+        }
+      }
+    } catch (SAXException | IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  private Medicine buildVitamins(Element medicineElement) {
+    VitaminsImpl vitamins = new VitaminsImpl();
+    vitamins.setName(getElementTextContent(medicineElement, NAME));
+    vitamins.setManufacturer(getElementTextContent(medicineElement, MANUFACTURER));
+    vitamins.setDosageForm(getElementTextContent(medicineElement, DOSAGE_FORM));
+    vitamins.setAmount(Integer.parseInt(getElementTextContent(medicineElement, AMOUNT)));
+    vitamins.setId(medicineElement.getAttribute(ID));
+    vitamins.setDosage(getElementTextContent(medicineElement, DOSAGE));
+    vitamins.setPrice(getElementBigDecimalContent(medicineElement, PRICE));
+    vitamins.setMedCertificateIssueDate(getElementDateContent(medicineElement, ISSUE_DATE));
+    vitamins.setMedCertificateExpiryDate(
+        getElementDateContent(medicineElement, EXPIRY_DATE));
+    vitamins.setSerial(medicineElement.getAttribute(SERIAL));
+    return vitamins;
+  }
+
+  private Medicine buildAntibiotics(Element medicineElement) {
+    AntibioticsImpl antibiotics = new AntibioticsImpl();
+    antibiotics.setName(getElementTextContent(medicineElement, NAME));
+    antibiotics.setManufacturer(getElementTextContent(medicineElement, MANUFACTURER));
+    antibiotics.setDosageForm(getElementTextContent(medicineElement, DOSAGE_FORM));
+    antibiotics.setAmount(Integer.parseInt(getElementTextContent(medicineElement, AMOUNT)));
+    antibiotics.setId(medicineElement.getAttribute(ID));
+    antibiotics.setDosage(getElementTextContent(medicineElement, DOSAGE));
+    antibiotics.setPrice(getElementBigDecimalContent(medicineElement, PRICE));
+    antibiotics.setMedCertificateIssueDate(getElementDateContent(medicineElement, ISSUE_DATE));
+    antibiotics.setMedCertificateExpiryDate(
+        getElementDateContent(medicineElement, EXPIRY_DATE));
+    return antibiotics;
+  }
+
+  private static String getElementTextContent(Element element, String elementName) {
+    NodeList nList = element.getElementsByTagName(elementName);
+    Node node = nList.item(0);
+    return node.getTextContent();
+  }
+
+  private static Date getElementDateContent(Element element, String elementName) {
+    NodeList nList = element.getElementsByTagName(elementName);
+    Node node = nList.item(0);
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
+    Date date = null;
+    try {
+      date = simpleDateFormat.parse(node.getTextContent());
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+    return date;
+  }
+
+  private static BigDecimal getElementBigDecimalContent(Element element, String elementName) {
+    NodeList nList = element.getElementsByTagName(elementName);
+    Node node = nList.item(0);
+    return new BigDecimal(node.getTextContent());
+  }
+}
---
+++ src/main/java/com/belyuk/second_project/builder/MedicineStaxBuilder.java
@@ -0,0 +1,118 @@
+package com.belyuk.second_project.builder;
+
+
+import com.belyuk.second_project.tag.MedicinesXmlTag;
+import com.belyuk.second_project.entity.impl.AntibioticsImpl;
+import com.belyuk.second_project.entity.Medicine;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+public class MedicineStaxBuilder {
+
+  private final Set<Medicine> medicineSet;
+  private final XMLInputFactory inputFactory;
+  private static final String DATE_PATTERN = "dd.MM.yyyy";
+
+  public MedicineStaxBuilder() {
+    inputFactory = XMLInputFactory.newInstance();
+    medicineSet = new HashSet<>();
+  }
+
+  public Set<Medicine> getMedicineSet() {
+    return medicineSet;
+  }
+
+  public void buildSetMedicines(String filename) {
+    XMLStreamReader reader;
+    String name;
+    try (FileInputStream inputStream = new FileInputStream(filename)) {
+      reader = inputFactory.createXMLStreamReader(inputStream);
+      while (reader.hasNext()) {
+        int type = reader.next();
+        if (type == XMLStreamConstants.START_ELEMENT) {
+          name = reader.getLocalName();
+          if (name.equals(MedicinesXmlTag.ANTIBIOTICS.getValue())) {
+            Medicine medicine = buildMedicine(reader);
+            medicineSet.add(medicine);
+          }
+        }
+      }
+    } catch (XMLStreamException | IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private Medicine buildMedicine(XMLStreamReader reader)
+      throws XMLStreamException {
+    AntibioticsImpl antibiotics = new AntibioticsImpl();
+
+    antibiotics.setId(reader.getAttributeValue(null, MedicinesXmlTag.ID.getValue()));
+    antibiotics.setName(reader.getAttributeValue(null,
+        MedicinesXmlTag.NAME.getValue()));
+
+    String name;
+    while (reader.hasNext()) {
+      int type = reader.next();
+      switch (type) {
+        case XMLStreamConstants.START_ELEMENT -> {
+          name = reader.getLocalName();
+          switch (MedicinesXmlTag.valueOf(name.toUpperCase())) {
+            case NAME -> antibiotics.setName(getXMLText(reader));
+            case MANUFACTURER -> antibiotics.setManufacturer(getXMLText(reader));
+            case DOSAGE_FORM -> antibiotics.setDosageForm(getXMLText(reader));
+            case ISSUE_DATE -> antibiotics.setMedCertificateIssueDate(getXMLDate(reader));
+            case EXPIRY_DATE -> antibiotics.setMedCertificateExpiryDate(getXMLDate(reader));
+            case DOSAGE -> antibiotics.setDosage(getXMLText(reader));
+            case AMOUNT -> antibiotics.setAmount(Integer.parseInt(getXMLText(reader)));
+            case ID -> antibiotics.setId(getXMLText(reader));
+//            case PRICE -> antibiotics.setPrice(getBigDecimalValue(reader)); //TODO
+          }
+        }
+        case XMLStreamConstants.END_ELEMENT -> {
+          name = reader.getLocalName();
+          if (MedicinesXmlTag.valueOf(name.toUpperCase()) == MedicinesXmlTag.ANTIBIOTICS) {
+            return antibiotics;
+          }
+        }
+      }
+    }
+    throw new XMLStreamException("Unknown element in tag <antibiotics>");
+  }
+
+  private String getXMLText(XMLStreamReader reader) {
+    String text = null;
+    try {
+      if (reader.hasNext()) {
+        reader.next();
+        text = reader.getText();
+      }
+    } catch (XMLStreamException e) {
+      e.printStackTrace();
+    }
+    return text;
+  }
+
+  private Date getXMLDate(XMLStreamReader reader) {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
+    Date date = null;
+    try {
+      if (reader.hasNext()) {
+        reader.next();
+        date = simpleDateFormat.parse(reader.getText());
+      }
+    } catch (XMLStreamException | ParseException e) {
+      e.printStackTrace();
+    }
+    return date;
+  }
+}
